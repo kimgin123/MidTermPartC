ea3fdf84526910f6637ad5124ab4b8e1cf6ef4c7
Gina Kim <iamsoggo@gmail.com>
2023-02-21 10:31:44 -0500

    branch name Gina_Kim

---
+++ src/midtermexam_versiona_extensioncode/Gina.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/* I created a branch named Gina_kim and a class named Gina.java */
+
+/**
+ *
+ * @author kimjeongrok
+ */
+public class Gina {
+    
+}
